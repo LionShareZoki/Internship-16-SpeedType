2c377ea800a4a2761ccc999881a1cdd76b694e29
LionShareZoki <pujic.zoran@gmail.com>
2024-03-18 20:35:40 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import PracticePage from "./pages/PracticePage/PracticePage";
+import { GameProvider } from "./components/GameContext";
+import RegularGameMode from "./pages/NewGamePage/GameModes/RegularGameMode";
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
